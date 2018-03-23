5376ae665351f0a65bea502038d30beb3ed7cf4f
Kevin D'Souza <ksdsouza@edu.uwaterloo.ca>
2018-03-22 22:22:05 -0400

    Using different properties structure

--- tohru/src/main/scala/io/github/ksdsouza/WebServer/WebServer.scala
+++ tohru/src/main/scala/io/github/ksdsouza/WebServer/WebServer.scala
@@ -18,7 +18,7 @@ object MainRouter {
 
 
 object WebServer {
-  val client = MongoClient("mongodb://localhost:27017")
+  val client = MongoClient(s"mongodb://${PropertyReader.MongoURL}:${PropertyReader.MongoPort}")
 
   def main(args: Array[String]) {
     implicit val system = ActorSystem("my-system")
@@ -32,14 +32,9 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val properties = new Properties()
-    properties.load(getClass.getResourceAsStream("Server.properties"))
-    val url = properties.getProperty("url")
-    val port = properties.getProperty("port").toInt
+    val bindingFuture = Http().bindAndHandle(MainRouter.routes, PropertyReader.ServerURL, PropertyReader.ServerPort)
 
-    val bindingFuture = Http().bindAndHandle(MainRouter.routes, url, port)
-
-    println(s"Server online at http://$url:$port/\nPress RETURN to stop...")
+    println(s"Server online at http://${PropertyReader.ServerURL}:${PropertyReader.ServerPort}/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
